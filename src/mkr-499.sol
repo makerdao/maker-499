8e82c273d6d1221f126977dbf50ef06f7b401b3c
Nikolai Mushegian <nikolai@nexusdev.us>
2017-08-20 17:19:41 -0400

    499 token/vault

--- src/mkr-499.sol
+++ src/mkr-499.sol
@@ -1,16 +1,23 @@
 pragma solidity ^0.4.15;
 
 import 'ds-chief/chief.sol';
+import 'ds-token/token.sol';
+import 'ds-vault/vault.sol';
 
 contract MakerUpdate499 {
     DSToken MKR;
+    DSChief MKRChief;
+    DSVault MakerFiniteVault;
+
     function run() {
         MKR = new DSToken();
         MKRChief = new DSChief();
+        MakerFiniteVault = new DSVault(MKR);
         // mkr token
         // mkr chief
         // mkr redeemer
         // mkr burner
         // weth token
+        // "makerdao" vault
     }
 }
