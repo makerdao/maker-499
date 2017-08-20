57e3f556984f6ef45d2dcaab0ae7725d36265089
Nikolai Mushegian <nikolai@nexusdev.us>
2017-08-20 17:08:45 -0400

    upgrade 499

--- src/mkr-499.sol
+++ src/mkr-499.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.4.15;
 import 'ds-chief/chief.sol';
 
 contract MakerUpdate499 {
+    DSToken MKR;
     function run() {
+        MKR = new DSToken();
+        MKRChief = new DSChief();
         // mkr token
         // mkr chief
         // mkr redeemer
