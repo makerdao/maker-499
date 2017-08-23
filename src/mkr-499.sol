6bfb430137ac19ad52bd27ded7e5fcaa35cff4f0
Nikolai Mushegian <nikolai@nexusdev.us>
2017-08-23 16:52:01 -0400

    mkr gov sketch

--- src/mkr-499.sol
+++ src/mkr-499.sol
@@ -2,18 +2,38 @@ pragma solidity ^0.4.15;
 
 import 'ds-chief/chief.sol';
 import 'ds-token/token.sol';
-import 'ds-vault/vault.sol';
+import 'ds-vault/multivault.sol';
 
-contract MakerUpdate499 {
+import 'ds-thing/thing.sol';
+
+contract Redeemer is DSThing {
+    ERC20 from;
+    DSToken to;
+    uint undo_deadline;
+    function Redeemer(ERC20 from, DSToken to, uint undo_deadline);
+    function redeem(uint128 wad) {
+        require( from.transferFrom(msg.sender, this, wad) ) );
+        to.push(msg.sender, wad);
+    }
+    function undo(uint128 wad) {
+        require( now < undo_deadline );
+        require( to.transfer(msg.sender, wad) );
+        to.pull(msg.sender, wad);
+    }
+}
+
+contract MakerUpdate499 is DSThing {
     DSToken MKR;
     DSChief MKRChief;
     DSVault DevFund;
 
     function run() {
         MKR = new DSToken();
-        MKRChief = new DSChief();
-        DevFund = new DSVault(MKR);
-        // mkr redeemer
+        MKR.mint(cast(1000000 ether));
+        MKRChief = new DSChief(MKR);
+        MKR.setAuthority(MKRChief);
+        MKR.setOwner(address(0));
+        DevFund = new DSMultiVault(MKR);
         // mkr burner
         // weth token
     }
