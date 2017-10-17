2e9e5db719f0999916d3d1b92e7b9ba2df677566
Ryan Casey <ryepdx@gmail.com>
2017-10-17 12:42:28 -0700

    Tests.

--- src/test.sol
+++ src/test.sol
@@ -15,20 +15,32 @@ contract TestAuthority is DSAuthority {
 
 contract MKRUser is TokenUser {
     function MKRUser(DSToken t) TokenUser(t) public {}
+
+    function doRedeem(Redeemer r) public {
+        r.redeem();
+    }
+
+    function doApprove(DSToken token, address recipient, uint amount)
+            public
+            returns (bool)
+    {
+        return token.approve(recipient, amount);
+    }
 }
 
 contract Maker499Test is DSTest {
     uint initialBalance = 1000000 ether; // 10**6 * 10**18
     MakerUpdate499 update;
-    MKRUser user1;
+    MKRUser user;
     DSAuthority authority;
     DSToken old_MKR;
 
     function setUp() public {
-        user1 = new MKRUser(old_MKR);
         old_MKR = new DSToken("MKR");
         old_MKR.mint(initialBalance);
-        old_MKR.push(user1, initialBalance);
+
+        user = new MKRUser(old_MKR);
+        old_MKR.push(user, initialBalance);
         authority = new TestAuthority();
     }
 
@@ -45,10 +57,25 @@ contract Maker499Test is DSTest {
         update = new MakerUpdate499(authority, old_MKR, deadline);
         assertEq(update.undo_deadline(), deadline);
         assertEq(address(update.old_MKR()), address(old_MKR));
+    }
+
+    function test_run() public {
+        uint deadline = now + 1 days;
+        update = new MakerUpdate499(authority, old_MKR, deadline);
+        update.run();
         assertTrue(address(update.MKR()) != address(old_MKR));
         assertTrue(address(update.MKR()) != address(0x0));
     }
 
-    function test_run() public {
+    function test_redeem() public {
+        uint deadline = now + 1 days;
+        update = new MakerUpdate499(authority, old_MKR, deadline);
+        update.run();
+
+        assertEq(old_MKR.balanceOf(user), initialBalance);
+        user.doApprove(old_MKR, update.redeemer(), initialBalance);
+        user.doRedeem(update.redeemer());
+        assertEq(old_MKR.balanceOf(user), 0);
+        assertEq(update.MKR().balanceOf(user), initialBalance);
     }
 }
